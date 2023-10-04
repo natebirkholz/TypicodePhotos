c41c3b1362458fcdc905f21796ad611bec7d5c1f
Nate Birkholz <nbirkholz@gmail.com>
2023-10-04 08:38:15 -0600

    Initial Commit

---
+++ TypicodePhotos/ContentView.swift
@@ -0,0 +1,20 @@
+// ContentView.swift
+// Created by Nate Birkholz
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TypicodePhotos/TypicodePhotosApp.swift
@@ -0,0 +1,13 @@
+// TypicodePhotosApp.swift
+// Created by Nate Birkholz
+
+import SwiftUI
+
+@main
+struct TypicodePhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
